0545ce4cd5cf02fff0f4bfc442b855393182be29
Kalbo <spankyelmalo@yahoo.es>
2025-10-17 12:45:26 +0200

    Update comment formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // *** CAMBIO NECESARIO AQUÍ ***
+  // *** CAMBIO NECESARIO AQUÍ  ***
   base: mode === 'production' ? '/VisorVariosGPX/' : '/', 
   // *****************************
 
